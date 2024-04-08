dc318d9eafa988382be0c708f4b451178018ebca
Rishi Senthil <122108111+Shortcircuit-163@users.noreply.github.com>
2024-04-08 10:06:50 +0530

    updated geometry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import *
 from tkinter.ttk import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
@@ -7,33 +6,40 @@ from tkinter import messagebox
 
 win = tk.Tk()
 win.title("Quicket")
-win.geometry('1600x720')
-win.resizable(height=False, width=False)
-p1 = PhotoImage(file='quicket.png')
+win.geometry('1366x768')
+p1 = tk.PhotoImage(file='quicket.png')
 win.iconphoto(True, p1)
 
-heading = Label(win, text="Quicket", font=('Times New Roman', 50, 'bold'))
-heading.grid()
-play_mode = Label(win, text='-Choose Game Mode-', font=('Century Gothic', 30, 'underline'))
-play_mode.place(relx=0.1,
-                rely=0.2,
-                anchor='nw')
+win.grid_columnconfigure(0, weight=1)
+win.grid_columnconfigure(1, weight=1)
+win.grid_columnconfigure(2, weight=1)
+win.grid_rowconfigure(0, weight=1)
+win.grid_rowconfigure(1, weight=1)
+win.grid_rowconfigure(2, weight=1)
+win.grid_rowconfigure(3, weight=1)
+win.grid_rowconfigure(4, weight=1)
 
-quicket_image = PhotoImage(file='quicket.png')
+
+heading = Label(win, text="Quicket", font=('Times New Roman', 50, 'bold'))
+heading.grid(row=0, column=1)
+
+quicket_image = tk.PhotoImage(file='quicket.png')
 quicket_label = Label(win, image=quicket_image)
-quicket_label.place(relx=0.1,
-                    rely=0,
-                    anchor='e')
+quicket_label.grid(row=1, column=1)
+
+play_mode = Label(win, text='-Choose Game Mode-', font=('Century Gothic', 30, 'underline'))
+play_mode.grid(row=2, column=1)
 
 style1_btn_gamemode = Style()
 style1_btn_gamemode.configure('A.TButton', font=('calibri', 50, 'bold', 'underline'), foreground='red')
-photo1 = PhotoImage(file='singleplayer.png')
+photo1 = tk.PhotoImage(file='singleplayer.png')
 btn1 = Button(win, image=photo1, style='A.TButton', command=None)
-btn1.grid(row=2, column=0, pady=2)
+btn1.grid(row=3, column=1)
 
 style2_btn_gamemode = Style()
 style2_btn_gamemode.configure('B.TButton', font=('calibri', 50, 'bold', 'underline'), foreground='orange')
-photo2 = PhotoImage(file='multiplayer.png')
+photo2 = tk.PhotoImage(file='multiplayer.png')
 btn2 = Button(win, image=photo2, style='B.TButton', command=None)
-btn2.grid(row=3, column=0, pady=2)
+btn2.grid(row=4, column=1)
+
 win.mainloop()
