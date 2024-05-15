20a5c78a5b8dcbb6e7955ce8dd1abc56047a8c7e
Rishi Senthil <122108111+Shortcircuit-163@users.noreply.github.com>
2024-05-15 10:41:37 +0530

    changes to player data

--- Singleplayer_start.py
+++ Singleplayer_start.py
@@ -4,11 +4,11 @@ from PIL import ImageTk, Image
 import csv
 
 
-def start_match_singleplayer(user):
+def start_match_singleplayer(name):
     
     sm = tk.Tk()
     sm.title("Quicket-Start Match")
-    sm.geometry('800x800')
+    sm.geometry('900x800')
     # sm.resizable(False, False)
     # sm.configure(background='light grey')
     p1 = tk.PhotoImage(file=r'images\home\quicket.png')
@@ -19,6 +19,130 @@ def start_match_singleplayer(user):
     sm.grid_columnconfigure(1, weight=1)
     sm.grid_columnconfigure(2, weight=1)
 
-    greeting = "Hello " + user + "!"
+    greeting = "Hello " + name + "!"
     heading = Label(sm, text=greeting, background='', font=('Times New Roman', 50, 'bold'))
     heading.grid(row=0, column=1)
+
+    player_info = tk.Frame(sm)
+    player_info.grid(row=1, column=1, pady = 100)
+
+    def return_playerdata():
+        data_path = r'Data\user_data.csv'
+        with open(data_path) as player_data:
+            user_reader = csv.reader(player_data, delimiter=',')
+            for row in user_reader:
+                if row[0] == name:
+                    batsmen_owned = row[3]
+                    bowlers_owned = row[4]
+                    wicket_keepers_owned = row[5]
+                    all_rounders_owned = row[6]
+                    wickets = row[7]               
+                    runs = row[8]              
+                    economy = row[9]              
+                    innings = row[10]               
+                    batting_average = row[11]               
+                    bowling_average = row[12]              
+                    batting_overs = row[13]               
+                    bowling_overs = row[14]              
+                    total_overs = row[15]
+                    all_data = [batsmen_owned, bowlers_owned, wicket_keepers_owned, all_rounders_owned, wickets, runs, economy, innings, batting_average, bowling_average, batting_overs, bowling_overs, total_overs]
+                    return all_data
+
+    
+    batsmen_owned, bowlers_owned, wicket_keepers_owned, all_rounders_owned, wickets, runs, economy, innings, batting_average, bowling_average, batting_overs, bowling_overs, total_overs = return_playerdata()[0], return_playerdata()[1], return_playerdata()[2], return_playerdata()[3], return_playerdata()[4], return_playerdata()[5], return_playerdata()[6], return_playerdata()[7], return_playerdata()[8], return_playerdata()[9], return_playerdata()[10], return_playerdata()[11], return_playerdata()[12]
+
+    bat = tk.Frame(player_info)
+    bat.grid(row=0, column=0, pady = 100)
+    batsmen = Label(bat, text='Batsmen', background='light grey', font=('Times New Roman', 20, 'bold'))
+    batsmen.grid(row=0, column=0)
+    batsmen_value = Label(bat, text=batsmen_owned, background='light grey', font=('Times New Roman', 30, 'bold'))
+    batsmen_value.grid(row=1, column=0)
+
+    bow = tk.Frame(player_info)
+    bow.grid(row=0, column=1, pady = 100)
+    bowlers = Label(bow, text='Bowlers', background='grey', font=('Times New Roman', 20, 'bold'))
+    bowlers.grid(row=0, column=0)
+    bowlers_value = Label(bow, text=bowlers_owned, background='grey', font=('Times New Roman', 30, 'bold'))
+    bowlers_value.grid(row=1, column=0)
+
+    wic = tk.Frame(player_info)
+    wic.grid(row=0, column=2, pady = 100)
+    wicket_keepers = Label(wic, text='Wicket Keepers', background='light grey', font=('Times New Roman', 20, 'bold'))
+    wicket_keepers.grid(row=0, column=0)
+    wicket_keepers_value = Label(wic, text=wicket_keepers_owned, background='light grey', font=('Times New Roman', 30, 'bold'))
+    wicket_keepers_value.grid(row=1, column=0)
+
+    all = tk.Frame(player_info)
+    all.grid(row=0, column=3, pady = 100)
+    all_rounders = Label(all, text='All Rounders', background='grey', font=('Times New Roman', 20, 'bold'))
+    all_rounders.grid(row=0, column=0)
+    all_rounders_value = Label(all, text=all_rounders_owned, background='grey', font=('Times New Roman', 30, 'bold'))
+    all_rounders_value.grid(row=1, column=0)
+
+    wice = tk.Frame(player_info)
+    wice.grid(row=0, column=4, pady = 100)
+    wickets_label = Label(wice, text='Wickets', background='light grey', font=('Times New Roman', 20, 'bold'))
+    wickets_label.grid(row=0, column=0)
+    wickets_value = Label(wice, text=wickets, background='light grey', font=('Times New Roman', 30, 'bold'))
+    wickets_value.grid(row=1, column=0)
+
+    run = tk.Frame(player_info)
+    run.grid(row=1, column=0, pady = 100)
+    runs_label = Label(run, text='Runs', background='grey', font=('Times New Roman', 20, 'bold'))
+    runs_label.grid(row=0, column=0)
+    runs_value = Label(run, text=runs, background='grey', font=('Times New Roman', 30, 'bold'))
+    runs_value.grid(row=1, column=0)
+
+    eco = tk.Frame(player_info)
+    eco.grid(row=1, column=1)
+    economy_label = Label(eco, text='Economy', background='light grey', font=('Times New Roman', 20, 'bold'))
+    economy_label.grid(row=0, column=0)
+    economy_value = Label(eco, text=economy, background='light grey', font=('Times New Roman', 30, 'bold'))
+    economy_value.grid(row=1, column=0)
+
+    inn = tk.Frame(player_info)
+    inn.grid(row=1, column=2)
+    innings_label = Label(inn, text='Innings', background='grey', font=('Times New Roman', 20, 'bold'))
+    innings_label.grid(row=0, column=0)
+    innings_value = Label(inn, text=innings, background='grey', font=('Times New Roman', 30, 'bold'))
+    innings_value.grid(row=1, column=0)
+
+    batt = tk.Frame(player_info)
+    batt.grid(row=1, column=3)
+    batting_avg = Label(batt, text='Batting Average', background='light grey', font=('Times New Roman', 20, 'bold'))
+    batting_avg.grid(row=0, column=0)
+    batting_avg_value = Label(batt, text=batting_average, background='light grey', font=('Times New Roman', 30, 'bold'))
+    batting_avg_value.grid(row=1, column=0)
+
+    bowl = tk.Frame(player_info)
+    bowl.grid(row=1, column=4)
+    bowling_avg = Label(bowl, text='Bowling Average', background='grey', font=('Times New Roman', 20, 'bold'))
+    bowling_avg.grid(row=0, column=0)
+    bowling_avg_value = Label(bowl, text=bowling_average, background='grey', font=('Times New Roman', 30, 'bold'))
+    bowling_avg_value.grid(row=1, column=0)
+
+    bat_avg = tk.Frame(player_info)
+    bat_avg.grid(row=2, column=1, pady = 100)
+    batting_overs_label = Label(bat_avg, text='Batting Overs', background='grey', font=('Times New Roman', 20, 'bold'))
+    batting_overs_label.grid(row=0, column=0)
+    batting_overs_value = Label(bat_avg, text=batting_overs, background='grey', font=('Times New Roman', 30, 'bold'))
+    batting_overs_value.grid(row=1, column=0)
+
+    bowl_avg = tk.Frame(player_info)
+    bowl_avg.grid(row=2, column=2)
+    bowling_overs_label = Label(bowl_avg, text='Bowling Overs', background='light grey', font=('Times New Roman', 20, 'bold'))
+    bowling_overs_label.grid(row=0, column=0)
+    bowling_overs_value = Label(bowl_avg, text=bowling_overs, background='light grey', font=('Times New Roman', 30, 'bold'))
+    bowling_overs_value.grid(row=1, column=0)
+
+    tot = tk.Frame(player_info)
+    tot.grid(row=2, column=3)
+    total_overs_label = Label(tot, text='Total Overs', background='grey', font=('Times New Roman', 20, 'bold'))
+    total_overs_label.grid(row=0, column=0)
+    total_overs_value = Label(tot, text=total_overs, background='grey', font=('Times New Roman', 30, 'bold'))
+    total_overs_value.grid(row=1, column=0)
+
+    sm.mainloop()
+
+# Batsmen owned,Bowlers owned,wicket keepers owned,all rounders owned,Wickets,Runs,Economy,Innings,
+# Batting Average,Bowling Average,Batting Overs,Bowling Overs,Total Overs
